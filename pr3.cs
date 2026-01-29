2da71c42057fe075ce7612f9e623f7bdf9dab61b
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-29 10:42:10 +0300

    pr3.cs

---
+++ pr3.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mIc
+{
+    namespace MegaGigaCompany
+    {
+        namespace CompanyTELEGRAM
+        {
+            public class KazinoBot
+            {
+                public string Dolgi(string MneNyjneVashiDenige)
+                {
+                    return "ЗВЁЗДЫ И ХОМЯК ЭТО СЧАСТЬЕ" + MneNyjneVashiDenige;
+                }
+            }
+            public class Durov
+            {
+                public double Francei(double money)
+                {
+                    return money;
+                }
+            }
+            public class Rab
+            {
+                public int num {  get; set; }
+            }
+        }
+        namespace CompanyMAX
+        {
+            public static class Parkovka
+            {
+                public static string LovitDajeNaParkovke()
+                {
+                    return "переходите на наш самый стабильный мессенджер";
+                }
+            }
+            public class MoshenikiKrugom
+            {
+                public double Gosuslugi(double money)
+                {
+                    return Math.Sqrt(money);
+                }
+            }
+            public class Rab
+            {
+                public int num { get; set; }
+                public string Birka { get; set; }
+            }
+        }
+    }
+}
